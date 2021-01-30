7a73809d6ceaa6102d8d65f5d1ceb00132c42fb6
Luís Soares <lsoares@gmail.com>
2021-01-30 13:32:58 +0000

    Get wrapped input

--- src/main/kotlin/GetByLabelText.kt
+++ src/main/kotlin/GetByLabelText.kt
@@ -1,7 +1,19 @@
 import org.jsoup.nodes.Element
 
 fun Element.getByLabelText(label: String): Element =
+    (getByFor(label) + getWrapped(label)).single() // TODO: don't filter by label twice
+
+private fun Element.getByFor(label: String) =
     getElementsByTag("label")
-        .single { it.text() == label }
-        .attr("for")
-        .let(::getElementById)
+        .asSequence()
+        .filter { it.text() == label }
+        .map { it.attr("for") }
+        .filter(String::isNotBlank)
+        .map(::getElementById)
+
+private fun Element.getWrapped(label: String) =
+    getElementsByTag("label")
+        .asSequence()
+        .filter { it.text() == label }
+        .map { it.select("input") }
+        .flatten()
--- src/test/kotlin/GetByLabelTextTest.kt
+++ src/test/kotlin/GetByLabelTextTest.kt
@@ -6,17 +6,47 @@ import org.junit.jupiter.api.Test
 class GetByLabelTextTest {
 
     @Test
-    fun `get by label basic`() {
-        val doc = Jsoup.parse("""
+    fun `get by label - for`() {
+        val doc = Jsoup.parse(
+            """
             <label for="email">Email address</label>
             <input id="email" />
             <label for="password">Password</label>
             <input id="password" />
-        """)
+        """
+        )
 
         val byLabelText = doc.getByLabelText("Email address")
 
         assertEquals("""<input id="email">""", byLabelText.toString())
     }
+
+    @Test
+    fun `get by label - wrapper`() {
+        val doc = Jsoup.parse(
+            """
+            <label>Username <input id="username" /></label>
+            <label>Password <input /></label>
+        """
+        )
+
+        val byLabelText = doc.getByLabelText("Username")
+
+        assertEquals("""<input id="username">""", byLabelText.toString())
+    }
+
+    // todo: filter only form elements
+//<input>
+//<label>
+//<select>
+//<textarea>
+//<button>
+//<fieldset>
+//<legend>
+//<datalist>
+//<output>
+//<option>
+//<optgroup>
+
 }
 
