43e160719b7e84ccf720913c316d89e7c178d19c
Luís Soares <lsoares@gmail.com>
2021-01-31 13:29:28 +0000

    Search by regex

--- src/main/kotlin/GetByLabelText.kt
+++ src/main/kotlin/GetByLabelText.kt
@@ -3,39 +3,42 @@ import org.jsoup.select.Elements
 import java.lang.Exception
 
 fun Element.queryByLabelText(
-    label: String,
+    text: String,
     exact: Boolean = true,
     selector: String? = null
 ): List<Element>? =
-    queryAllByLabelText(label, exact, selector)
+    queryAllByLabelText(text, exact, selector)
         .takeIf { it.isNotEmpty() }
         ?.also { check(it.size == 1) { throw UndefinedResult() } }
 
 fun Element.getAllByLabelText(
-    label: String,
+    text: String,
     exact: Boolean = true,
     selector: String? = null
 ): List<Element> =
-    queryAllByLabelText(label, exact, selector)
+    queryAllByLabelText(text, exact, selector)
         .takeIf { it.isNotEmpty() }
         ?: throw UndefinedResult()
 
 fun Element.getByLabelText(
-    label: String,
+    text: String,
     exact: Boolean = true,
     selector: String? = null
 ): Element =
-    queryAllByLabelText(label, exact, selector)
+    queryAllByLabelText(text, exact, selector)
         .singleOrNull()
         ?: throw UndefinedResult()
 
 fun Element.queryAllByLabelText(
-    label: String,
+    text: String? = null,
     exact: Boolean = true,
-    selector: String? = null
+    selector: String? = null,
+    textRegex: Regex? = null,
 ): List<Element> =
     getElementsByTag("label")
-        .filter { it.initialMatcher(label, exact) }
+        .asSequence()
+        .filter { it.textMatcher(text, exact) }
+        .filter { it.textRegexMatcher(textRegex) }
         .map {
             it.children() + getByAriaLabelledBy(it) + listOfNotNull(getByFor(it))
         }
@@ -43,12 +46,20 @@ fun Element.queryAllByLabelText(
         .filter {
             it.tagName() in selector ?: "input, select, textarea, button, output"
         }
+        .toList()
 
-private fun Element.initialMatcher(label: String, exact: Boolean) =
-    when (exact) {
-        true -> text() == label
-        false -> text().toLowerCase().contains(label.toLowerCase())
-    }
+private fun Element.textRegexMatcher(textRegex: Regex?) =
+    textRegex?.let {
+        text().matches(it)
+    } ?: true
+
+private fun Element.textMatcher(text: String?, exact: Boolean) =
+    text?.let {
+        when (exact) {
+            true -> text() == it
+            false -> text().toLowerCase().contains(it.toLowerCase())
+        }
+    } ?: true
 
 private fun Element.getByAriaLabelledBy(label: Element) =
     label.attr("id")
--- src/test/kotlin/QueryAllByLabelTextTest.kt
+++ src/test/kotlin/QueryAllByLabelTextTest.kt
@@ -1,4 +1,6 @@
 import org.jsoup.Jsoup
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
@@ -11,5 +13,29 @@ class QueryAllByLabelTextTest {
         val byLabelText = doc.queryAllByLabelText("Email")
 
         assertTrue(byLabelText.isEmpty())
+    }
+
+
+    @Test
+    fun `search by regex`() {
+        val doc = Jsoup.parse(
+            """
+            <label for='id1'>
+              User id
+            </label>
+            <input id='id1' />
+            <label>
+              Username
+              <textarea></textarea>
+            </label> 
+        """
+        )
+
+        val byLabelText = doc.queryAllByLabelText(textRegex = "User.*".toRegex())
+
+        assertEquals(
+            """[<input id="id1">, <textarea></textarea>]""",
+            byLabelText.toString()
+        )
     }
 }
