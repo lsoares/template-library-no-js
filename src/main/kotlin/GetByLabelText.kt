72d72e540377b3a865c7b76eaa0c0e49c4d671a2
Luís Soares <lsoares@gmail.com>
2021-01-31 12:45:25 +0000

    implement get all by label text

--- src/main/kotlin/GetByLabelText.kt
+++ src/main/kotlin/GetByLabelText.kt
@@ -2,21 +2,36 @@ import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 import java.lang.Exception
 
+fun Element.getAllByLabelText(
+    label: String,
+    exact: Boolean = true,
+    selector: String? = null
+): List<Element> =
+    filterByLabel(label, exact, selector)
 
 fun Element.getByLabelText(
     label: String,
     exact: Boolean = true,
     selector: String? = null
 ): Element =
+    filterByLabel(label, exact, selector)
+        .singleOrNull {
+            it.tagName() in selector ?: "input, select, textarea, button, output"
+        }
+        ?: throw UndefinedResult()
+
+
+private fun Element.filterByLabel(
+    label: String,
+    exact: Boolean = true,
+    selector: String? = null
+): List<Element> =
     getElementsByTag("label")
         .filter { it.initialMatcher(label, exact) }
         .map {
             it.children() + getByAriaLabelledBy(it) + listOfNotNull(getByFor(it))
         }
         .flatten()
-        .singleOrNull {
-            it.tagName() in selector ?: "input, select, textarea, button, output" }
-        ?: throw UndefinedResult()
 
 private fun Element.initialMatcher(label: String, exact: Boolean) =
     when (exact) {
---
+++ src/test/kotlin/GetAllByLabelTextTest.kt
@@ -0,0 +1,24 @@
+import org.jsoup.Jsoup
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class GetAllByLabelTextTest {
+
+    @Test
+    fun `gets all labels`() {
+        val doc = Jsoup.parse(
+            """
+            <label for="email1">Email address</label>
+            <input id="email1" />
+            <label>Email address<input id="email2" /></label>
+        """
+        )
+
+        val byLabelText = doc.getAllByLabelText("Email address")
+
+        assertEquals(
+            """[<input id="email1">, <input id="email2">]""",
+            byLabelText.toString()
+        )
+    }
+}
