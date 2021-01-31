d9e62b62df8098a754f77d6ba1850ca47eeab5fc
Luís Soares <lsoares@gmail.com>
2021-01-31 13:40:49 +0000

    remove untested code

--- src/main/kotlin/GetByLabelText.kt
+++ src/main/kotlin/GetByLabelText.kt
@@ -2,30 +2,18 @@ import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 import java.lang.Exception
 
-fun Element.queryByLabelText(
-    text: String,
-    exact: Boolean = true,
-    selector: String? = null
-): List<Element>? =
-    queryAllByLabelText(text, exact, selector)
+fun Element.queryByLabelText(text: String): List<Element>? =
+    queryAllByLabelText(text)
         .takeIf { it.isNotEmpty() }
         ?.also { check(it.size == 1) { throw UndefinedResult() } }
 
-fun Element.getAllByLabelText(
-    text: String,
-    exact: Boolean = true,
-    selector: String? = null
-): List<Element> =
-    queryAllByLabelText(text, exact, selector)
+fun Element.getAllByLabelText(text: String): List<Element> =
+    queryAllByLabelText(text)
         .takeIf { it.isNotEmpty() }
         ?: throw UndefinedResult()
 
-fun Element.getByLabelText(
-    text: String,
-    exact: Boolean = true,
-    selector: String? = null
-): Element =
-    queryAllByLabelText(text, exact, selector)
+fun Element.getByLabelText(text: String): Element =
+    queryAllByLabelText(text)
         .singleOrNull()
         ?: throw UndefinedResult()
 
