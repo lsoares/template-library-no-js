69793b6eb9f2bbf0448260f3955670eb2bc6f42d
Luís Soares <lsoares@gmail.com>
2021-02-01 23:39:43 +0000

    Deal with no results

---
+++ src/test/kotlin/bytext/NoResultsTest.kt
@@ -0,0 +1,38 @@
+package bytext
+
+import UndefinedResult
+import org.jsoup.Jsoup
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class NoResultsTest {
+
+    @Test
+    fun `returns null when no results match`() {
+        val doc = Jsoup.parse("<span />")
+
+        val queryByText = doc.queryByText("Email")
+
+        assertNull(queryByText)
+    }
+
+    @Test
+    fun `returns empty array when no results match`() {
+        val doc = Jsoup.parse("<span />")
+
+        val byLabelText = doc.queryAllByText(text = "Email")
+
+        assertTrue(byLabelText.isEmpty())
+    }
+
+    @Test
+    fun `fails when no results match`() {
+        val doc = Jsoup.parse("<span />")
+
+        val getByText = { doc.getByText("Email") }
+        val getAllByText = { doc.getAllByText("Email") }
+
+        assertThrows(UndefinedResult::class.java) { getByText() }
+        assertThrows(UndefinedResult::class.java) { getAllByText() }
+    }
+}
