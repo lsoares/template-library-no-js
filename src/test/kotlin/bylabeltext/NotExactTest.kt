ff62c7d166ee5d9e5f7f10e0e420f50ce9fb7117
Luís Soares <lsoares@gmail.com>
2021-02-01 13:26:32 +0000

    search not exact with selector

--- src/test/kotlin/bylabeltext/NotExactTest.kt
+++ src/test/kotlin/bylabeltext/NotExactTest.kt
@@ -26,5 +26,25 @@ class NotExactTest {
         assertEquals("email", queryAllByLabelText.single().id())
     }
 
-    // TODO test with selector
+    @Test
+    fun `not exact with selector`() {
+        val doc = Jsoup.parse(
+            """
+            <label for="email">Email address</label>
+            <input id="email" />
+            <label for="email2">Email address</label>
+            <textarea id="email2" />
+        """
+        )
+
+        val getByLabelText = doc.getByLabelText("email", exact = false, selector = "input")
+        val queryByLabelText = doc.queryByLabelText("email", exact = false, selector = "input")
+        val getAllByLabelText = doc.getAllByLabelText("email", exact = false, selector = "input")
+        val queryAllByLabelText = doc.queryAllByLabelText("email", exact = false, selector = "input")
+
+        assertEquals("email", getByLabelText.id())
+        assertEquals("email", queryByLabelText?.id())
+        assertEquals("email", getAllByLabelText.single().id())
+        assertEquals("email", queryAllByLabelText.single().id())
+    }
 }
